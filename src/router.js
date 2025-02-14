84bdda1ed5ec2c9a88b25749f80a6d9366e6f263
4levy <100963276+4levy@users.noreply.github.com>
2025-02-14 17:42:34 +0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,1 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import NotFound from "./components/NotFound.vue";
 
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: App,
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
