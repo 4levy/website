c781f625c0eee5d62497fe0b56d43d62c8d56cf2
4levy <100963276+4levy@users.noreply.github.com>
2025-02-23 18:28:03 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -56,6 +56,22 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/i,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next/static/media/",
+            outputPath: "static/media/",
+            name: "[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
